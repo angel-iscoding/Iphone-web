aa708a3c1224a382357120fd1e3040217106f999
angel-iscoding <angel.iscoding@gmail.com>
2025-01-04 00:57:54 -0500

    Features Agregado y terminado con modificaciones en las animaciones

--- apple-website/src/App.jsx
+++ apple-website/src/App.jsx
@@ -2,6 +2,7 @@ import NavBar from "./components/NavBar"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Model from "./components/Model"
+import Features from "./components/Features"
 
 const App = () => {
 
@@ -11,6 +12,7 @@ const App = () => {
       <Hero/>
       <Highlights/>
       <Model/>
+      <Features/>
     </main>
   )
 }
